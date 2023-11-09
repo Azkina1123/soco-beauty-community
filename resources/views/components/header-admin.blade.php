2b0fefaf6a2f66f47aa2781ba7b5e3d7668b8b85
Ekleccia Reydianto <93103544+EklecciaR27@users.noreply.github.com>
2023-11-09 22:54:11 +0800

    Update header-admin.blade.php
    update header-admin

--- resources/views/components/header-admin.blade.php
+++ resources/views/components/header-admin.blade.php
@@ -5,23 +5,7 @@
     </div>
 
     <div class="w-[50%] flex justify-end items-center">
-        {{-- <a href="{{ route('admin.home') }}" class="menu-home me-5">
-            @include('components.text-btn', ['label' => 'Home', 'type' => 'button'])
-        </a>
-
-        <a class="menu-skincares me-5">
-            @include('components.text-btn', ['label' => 'Account', 'type' => 'button'])
-        </a>
-
-        <a class="menu-skincares me-5">
-            @include('components.text-btn', ['label' => 'Produk', 'type' => 'button'])
-        </a>
-
-        <a class="menu-skincares me-5">
-            @include('components.text-btn', ['label' => 'Review', 'type' => 'button'])
-        </a> --}}
-
-        <a href="{{ route('user.profile') }}"
+        <a href="{{ route('user.profile', ['username' => Auth::user()->username]) }}"
             class="menu-profile flex justify-end items-center text-light-slate-grey hover:text-cool-blue-hover focus:text-cool-blue-click">
             <p class="">{{ Auth::user()->username }}</p>
             <div class="rounded-full bg-black w-12 h-12 ms-5"></div>
