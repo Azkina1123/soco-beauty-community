519f88129643667afcd16a7a7462a9087d317aea
Ekleccia Reydianto <93103544+EklecciaR27@users.noreply.github.com>
2023-11-15 14:35:58 +0800

    update footer
    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,85 @@
-<div class="h-[300px] bg-cyan-blue flex flex-col items-center justify-center">
+{{-- <div class="h-[300px] bg-cyan-blue flex flex-col items-center justify-center">
     <h1 class="text-2xl">SOCO </h1>
     <h2>Beauty Community</h2>
-    <div class="grid grid-cols-3 gap-5 mt-8">
-        <div class="rounded-full bg-black w-12 h-12"></div>
-        <div class="rounded-full bg-black w-12 h-12"></div>
-        <div class="rounded-full bg-black w-12 h-12"></div>
+</div> --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+<section class="footer">
+    <div class="container bg-cyan-blue max-w-full mx-auto py-10 px-14">
+        <ul class="grid grid-cols-1 items-start gap-5 pb-5 md:grid-cols-2 lg:grid-cols-4 ">
+            <li>
+                <div class="space-y-6 my-2">
+                    <a href="" class="text-4xl font-oswald uppercase">
+                        FIND US :
+                    </a>
+
+                    <p class="text-xs">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
+                </div>
+            </li>
+
+            <li>
+                <div class="flex flex-col px-16 gap-3">
+                    <h3 class="text-lg uppercase font-oswald">SUPPORT</h3>
+                    <a href="" class="text-xs hover:bg-yellow-500">ABOUT US</a>
+                    <a href="" class="text-xs  hover:bg-yellow-500">CONTACT US</a>
+                    <a href="" class="text-xs hover:bg-yellow-500">PRIVACY POLICY</a>
+                    <a href="" class="text-xs  hover:bg-yellow-500">FAQ's</a>
+                </div>
+            </li>
+
+            <li class="space-y-8">
+                <div class="space-y-2 px-12">
+                    <h3 class="text-lg uppercase font-oswald">phone</h3>
+
+                    <p class="flex items-center gap-2 text-xs">
+                        <i class="fa-solid fa-phone text-lg"></i>
+                        +628 1029319103
+                    </p>
+                </div>
+
+                <div class="space-y-2 px-12">
+                    <h3 class="text-lg uppercase font-oswald">email</h3>
+
+                    <p class="flex items-center gap-2 text-xs">
+                        <i class="fa-solid fa-envelope text-lg"></i>
+                        soco.info@gmail.com
+                    </p>
+                </div>
+
+
+            </li>
+
+            <li class="space-y-8">
+                <div class="space-y-2 px-12">
+                    <h3 class="text-lg uppercase font-oswald">address</h3>
+
+                    <p class="flex items-center gap-2 text-xs">
+                        <i class="fa-solid fa-location-dot text-lg"></i>
+                        Wall Street, Jakarta Baru
+                    </p>
+                </div>
+
+                <div class="space-y-2 px-12">
+                    <h3 class="text-lg uppercase font-oswald">follow us</h3>
+
+                   <div class="space-x-3">
+                        <i class="fa-brands fa-facebook-f text-lg cursor-pointer"></i>
+                        <i class="fa-brands fa-twitter text-lg cursor-pointer"></i>
+                        <i class="fa-brands fa-square-instagram text-lg cursor-pointer"></i>
+                   </div>
+                </div>
+
+
+            </li>
+
+        </ul>
+
+        <div class="flex flex-col items-center py-5 md:flex-grow md:justify-between">
+            <h1 class="text-2xl font-semibold">SOCO </h1>
+            <h2>Beauty Community</h2>
+        </div>
     </div>
-</div>
+</section>
